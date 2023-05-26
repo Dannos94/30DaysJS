ce2e93bb33b1ec3a46f3578f8055c20e5f0457ee
Dannos94 <111809408+Dannos94@users.noreply.github.com>
2023-05-26 11:15:01 -0500

    Add files via upload

---
+++ Día 1/variable.js
@@ -0,0 +1,15 @@
+let var1="";
+let var2=false;
+let var3;
+let var4=null;
+
+let var5="Pollo";
+let var6=36;
+let var7=true;
+let var8="Color 54";
+
+let names="",lastName="",age=0,estCivil="",pais="";
+
+
+
+
